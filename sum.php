2d83113717e687d53464c07735510428fd183ae9
sakuma <srff7cloud@yahoo.co.jp>
2020-02-05 05:00:27 +0000

    sum.php

--- sum.php
+++ sum.php
@@ -5,11 +5,13 @@ function nibai($x){
 }
 echo nibai(10).PHP_EOL;
 
+
 //2.$a と $b を仮引数に持ち、　$a と $b　を足した結果を返す関数を作成してください。
 function sum($a, $b) {
     return $a + $b;
 }
 echo sum(1,2).PHP_EOL;
+
 
 //3.$arr という配列の仮引数を持ち、数値が入った配列array(1, 3, 5 ,7, 9) を渡すとその要素をすべてかけた結果を返す関数を作成してください。
 function number($arr) {
@@ -22,20 +24,20 @@ function number($arr) {
 echo number(array(1, 3, 5, 7, 9)).PHP_EOL;
 
 
+//4.【応用】　次のプログラムは、配列の中で一番大きい値を返す max_array という関数を実装しようとしています。途中の部分を完成させてください。
 function max_array($arr){
 // とりあえず配列の最初の要素を一番大きい値とする
 $max_number = $arr[0];
-foreach($arr as $a){
+    foreach($arr as $a){
 //どうしたらいいかわからない・・・・    
     if($max_number < $a){
         $max_number = $a;
     }
+    }
+    return $max_number;
 }
-
-return $max_number;
-}
- 
 echo max_array(array(1, 2 ,3 , 4, 5, 100)).PHP_EOL;
+
 
 //5.次のビルトイン関数の用途、使い方を調べて実際に使ってみてください。
 
@@ -64,16 +66,3 @@ print_r($result).PHP_EOL;
 //date・・・・指定された日時を任意の形式でフォーマットし、日付文字列を返す関数
 echo "today " . date('Y-m-d', time(0, 0, 0, 5, 6, 1234)).PHP_EOL;
 echo "today " . date('Y-m-d', mktime(0, 0, 0, 5, 6, 1234)).PHP_EOL;
-
-
-
-
-
-
-
-
-
-
-
-
-
